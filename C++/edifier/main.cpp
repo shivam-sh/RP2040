555587d6b8555bbf7a9474dc63ecfa71514c5d4e
Shivam Sharma <shivam.sharm@icloud.com>
2021-09-01 16:12:11 -0400

    Minor tweaks and labels

--- C++/edifier/main.cpp
+++ C++/edifier/main.cpp
@@ -1,6 +1,5 @@
 #include "pico/stdlib.h"
 
-const uint LED = 13;
 const uint VOL_UP_GPIO = 12;
 const uint MUTE_GPIO = 11;
 const uint VOL_DOWN_GPIO = 10;
@@ -10,27 +9,36 @@ volatile bool mute = false;
 volatile bool vol_down = false;
 
 void irq_handler(uint gpio, uint32_t events) {
+    // Track the pin states during the interrupt
     vol_up = gpio_get(VOL_UP_GPIO);
     mute = gpio_get(MUTE_GPIO);
     vol_down = gpio_get(VOL_DOWN_GPIO);
 }
 
 int main() {
+    // Temporary LED for debugging
+    const uint LED = 13;
     gpio_init(LED);
+    gpio_set_dir(LED, GPIO_OUT);
+
+    // Initialize GPIO
     gpio_init(VOL_UP_GPIO);
     gpio_init(VOL_DOWN_GPIO);
     gpio_init(MUTE_GPIO);
 
-    gpio_set_dir(LED, GPIO_OUT);
+    // Set GPIO direction
     gpio_set_dir(VOL_UP_GPIO, GPIO_IN);
     gpio_set_dir(VOL_DOWN_GPIO, GPIO_IN);
     gpio_set_dir(MUTE_GPIO, GPIO_IN);
 
+    // Set GPIO interrupts on change
     gpio_set_irq_enabled_with_callback(VOL_UP_GPIO, 0b1100, true, irq_handler);
     gpio_set_irq_enabled_with_callback(MUTE_GPIO, 0b1100, true, irq_handler);
     gpio_set_irq_enabled_with_callback(VOL_DOWN_GPIO, 0b1100, true, irq_handler);
 
+    // Main loop
     while (true) {
+        // Flash patterns to show current button states
         if (vol_up) {
             gpio_put(LED, 1);
             sleep_ms(50);
